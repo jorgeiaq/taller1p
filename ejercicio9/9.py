a0c6a552f064ae2a67902c2568a262a4adcbee4b
jorgeiaq <132301914+jorgeiaq@users.noreply.github.com>
2023-05-02 14:57:17 -0500

    Delete 9.py

--- ejercicio9/9.py
+++
@@ -1,6 +0,0 @@
-# 1 dolar vale 0.84 euros
-change = 0.91
-dollar_value = float(input("introduzca la cantidad que quiere convertir"))
-conversion_euro = dollar_value * change
-print("la conversion da:", conversion_euro)
-# esto dado a que el euro cambio constantemente
